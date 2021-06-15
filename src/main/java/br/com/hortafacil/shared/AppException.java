0a120228caa47c31c7dd49530b29427e6944bccb
SamStalschus <samuel5stalschus@gmail.com>
2021-06-15 01:48:29 -0300

    Fix: Modify appError

--- src/main/java/br/com/hortafacil/shared/AppException.java
+++ src/main/java/br/com/hortafacil/shared/AppException.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ResponseStatus(NOT_FOUND)
-public class AddressNotFoundException extends RuntimeException {
-  public AddressNotFoundException(String exception) {
+public class AppException extends RuntimeException {
+  public AppException(String exception) {
     super(exception);
   }
 }
